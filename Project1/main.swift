e7914406bcd9819e78466276a2da68f25ed3e663
Fernando Arenas <fernando@vogcalgaryappdeveloper.com>
2020-10-06 10:35:00 -0600

    testing git

---
+++ Project1/main.swift
@@ -0,0 +1,185 @@
+//
+//  main.swift
+//  Project1
+//
+//  Created by Fernando Arenas on 2020-10-05.
+//  Copyright © 2020 Fernando Arenas. All rights reserved.
+//
+
+import Foundation
+
+//print("Please enter n")
+//
+//var n = Int(readLine() ?? "0") ?? 0
+//
+//print("Please enter m")
+//var m = Int(readLine() ?? "0") ?? 0
+
+
+//print("\(n) + \(m) = \(n+m)")
+//
+//print("\(n) - \(m) = \(n-m)")
+//
+//print("\(n) * \(m) = \(n*m)")
+//
+//if m == 0 {
+//    print("You cannot divide by 0")
+//
+//}else {
+//    print("\(n) / \(m) = \(n/m)")
+//
+//    print("\(n) % \(m) = \(n%m)")
+//}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//print("please input n")
+//
+//var n = Int(readLine() ?? "0")!
+//
+//print("Please input m")
+//
+//var m = Int(readLine() ?? "0")!
+//
+//if (n>m){
+//    print("\(n) > \(m)")
+//}
+//else if (n<m){
+//    print("\(n) < \(m)")
+//}
+//else{
+//    print("\(n) == \(m)")
+//}
+
+
+//print ("Input your age:")
+//
+//var age:Int = 0
+//
+//while (age != 23){
+//
+//    age = Int(readLine() ?? "0")!
+//
+//    if (age >= 0 && age <= 2)
+//    {
+//        print("You are an infant")
+//    } else if (age > 2 && age <= 13)
+//    {
+//        print("You are a kid")
+//    } else if(age > 13 && age <= 18)
+//    {
+//        print("You are a teenager")
+//    } else if(age > 18 && age <= 60)
+//    {
+//        print("You are an adult")
+//    } else {
+//        print("You are senior, you will die soon")
+//    }
+//}
+
+//var temp:Int
+//
+//print ("Input 1st number")
+//var first = Int(readLine() ?? "0")!
+//
+//print ("Input 2nd number")
+//var second = Int(readLine() ?? "0")!
+//
+//print ("Input 3rd number")
+//var third = Int(readLine() ?? "0")!
+//
+//print ("Input 4th number")
+//var fourth = Int(readLine() ?? "0")!
+//
+//print ("Input 5th number")
+//var fifth = Int(readLine() ?? "0")!
+//
+////First
+//if (second<=first){
+//    temp = first
+//    first = second
+//    second = temp
+//}
+//if (third<=first){
+//    temp = first
+//    first = third
+//    third = temp
+//}
+//if (fourth<=first){
+//    temp = first
+//    first = fourth
+//    fourth = temp
+//}
+//if (fifth<=first){
+//    temp = first
+//    first = fifth
+//    fifth = temp
+//}
+//
+////Second
+//if (third<=second){
+//    temp = second
+//    second = third
+//    third = temp
+//}
+//if (fourth<=second){
+//    temp = second
+//    second = fourth
+//    fourth = temp
+//}
+//if (fifth<=second){
+//    temp = second
+//    second = fifth
+//    fifth = temp
+//}
+//
+////third
+//if (fourth<=third){
+//    temp = third
+//    third = fourth
+//    fourth = temp
+//}
+//if (fifth<=third){
+//    temp = third
+//    third = fifth
+//    fifth = temp
+//}
+//
+////fourth
+//if (fifth<=fourth){
+//    temp = fourth
+//    fourth = fifth
+//    fifth = temp
+//}
+//
+//print("\(first),\(second),\(third),\(fourth),\(fifth)")
+
+
+
+
+print ("Input a")
+var a = Double(readLine() ?? "0")!
+
+print ("Input b")
+var b = Double(readLine() ?? "0")!
+
+print ("Input c")
+var c = Double(readLine() ?? "0")!
+
+var answerNeg:Double = (-1)*b+(-1)*(5.squareRoot())
