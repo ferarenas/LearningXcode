88042b691ff98e63f3b166d7512c5111db463af0
Fernando Arenas <fernando@vogcalgaryappdeveloper.com>
2020-10-11 23:31:52 -0600

    Removed the old assignments and Fixed the assignment numbers to match with the ones on the Word Document

--- Project1/main.swift
+++ Project1/main.swift
@@ -11,31 +11,17 @@ import Foundation
 var choice:Int = 0
 
 while (choice != 99){
-    
-    //Explains what the program does
-//    print("Which Assignment would you like to check? \n")
-//    print("1. Input 5 numbers and Order them from Min to Max")
-//    print("2. Solve Cuadratic Ecuation ax^2-bx+c=0")
-//    print("3. Solve 2 equations with 3 variables")
-//    //Loops
-//    print("LOOPS:")
-//    print("4. Count up to a natural number")
-//    print("5. Display Multiplication Table")
-//    print("6. Add all natural numbers up to n")
-//    print("7. Calculate average from all numbers from 1 to n")
-//    print("8. Sum of odd numbers to n")
-//    print("9. Average of all even numbers")
-//
-//    print("99. Exit \n")
-//    print("Selection= ",terminator:"")
+
+    print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
+    print("Which Assignment would you like to check?")
+    print("- \"99\" to Exit \n")
+    print("Selection= ",terminator:"")
     
     choice =
-    20
-    print()
-    //Int(readLine() ?? "0")!
+//   20; print()
+        Int(readLine() ?? "0") ?? 0
     
     switch choice {
-        
     case 1: assignment1()
     case 2: assignment2()
     case 3: assignment3()
@@ -52,11 +38,7 @@ while (choice != 99){
     case 14: assignment14()
     case 15: assignment15()
     case 16: assignment16()
-    case 17: assignment17()
-    case 18: assignment18()
-    case 19: assignment19()
-    case 20: assignment20()
-        
+
     case 99:
         print()
         print("Thank you!")
@@ -72,172 +54,8 @@ func enterToContinue(){
     _ = readLine()
 }
 
-func assignment1(){
-    
-    var temp:Int
-    
-    print ("\nInput 1st number")
-    var first = Int(readLine() ?? "0") ?? 0
-    print ("Input 2nd number")
-    var second = Int(readLine() ?? "0") ?? 0
-    print ("Input 3rd number")
-    var third = Int(readLine() ?? "0") ?? 0
-    print ("Input 4th number")
-    var fourth = Int(readLine() ?? "0") ?? 0
-    print ("Input 5th number")
-    var fifth = Int(readLine() ?? "0") ?? 0
-    
-    //First
-    if (second<=first){
-        temp = first
-        first = second
-        second = temp
-    }
-    if (third<=first){
-        temp = first
-        first = third
-        third = temp
-    }
-    if (fourth<=first){
-        temp = first
-        first = fourth
-        fourth = temp
-    }
-    if (fifth<=first){
-        temp = first
-        first = fifth
-        fifth = temp
-    }
-    
-    //Second
-    if (third<=second){
-        temp = second
-        second = third
-        third = temp
-    }
-    if (fourth<=second){
-        temp = second
-        second = fourth
-        fourth = temp
-    }
-    if (fifth<=second){
-        temp = second
-        second = fifth
-        fifth = temp
-    }
-    
-    //Third
-    if (fourth<=third){
-        temp = third
-        third = fourth
-        fourth = temp
-    }
-    if (fifth<=third){
-        temp = third
-        third = fifth
-        fifth = temp
-    }
-    
-    //Fourth
-    if (fifth<=fourth){
-        temp = fourth
-        fourth = fifth
-        fifth = temp
-    }
-    
-    print("\n Numbers in order are: \(first),\(second),\(third),\(fourth),\(fifth)")
-    
-}
 
-func assignment2(){
- 
-    print ("a=",terminator:"")
-    let a = Double(readLine() ?? "0")!
-    
-    print ("b=",terminator:"")
-    let b = Double(readLine() ?? "0")!
-    
-    print ("c=",terminator:"")
-    let c = Double(readLine() ?? "0")!
-    
-    let x:Double = ((-1*b)-(((b*b)-(4*a*c)).squareRoot()))/(2*a)
-    
-    let y:Double = ((-1*b)+(((b*b)-(4*a*c)).squareRoot()))/(2*a)
-    
-    if (x.isNaN){
-        print("\n x1 is not a number")
-    }
-    if (y.isNaN){
-        print("\n x2 is not a number")
-    }
-    else {
-        print("\nThe answers are:")
-        print("x1 = \((round(x*1000))/1000)")
-        print("x2 = \((round(y*1000))/1000)")
-    }
-    
-}
-
-func assignment3(){
-    
-    //Variables to temporarily hold values
-    var xTemp:Double = 0
-    var zTemp:Double = 0
-    var commonDen:Double = 0
-    
-    //Variables to get answers
-    var x:Double = 0
-    var y:Double = 0
-    var z:Double = 0
-    
-   print ("\nInput x1 number")
-   let xOne = Double(readLine() ?? "0") ?? 0
-   print ("Input y1 number")
-   let yOne = Double(readLine() ?? "0") ?? 0
-   print ("Input z1 number")
-   var zOne = Double(readLine() ?? "0") ?? 0
-   print ("Input x2 number")
-   let xTwo = Double(readLine() ?? "0") ?? 0
-   print ("Input y2 number")
-   let yTwo = Double(readLine() ?? "0") ?? 0
-   print ("Input z2 number")
-   let zTwo = Double(readLine() ?? "0") ?? 0
-    
-    //Solve first equation
-    if (yOne>yTwo){
-        commonDen = -(yOne/yTwo)
-        
-        xTemp = xTwo*commonDen
-        zTemp = zTwo*commonDen
-        
-        x=xOne+xTemp
-        z=zOne+zTemp
-        
-    }else{
-        commonDen = -(yTwo/yOne)
-        
-        xTemp = xOne*commonDen
-        zTemp = zOne*commonDen
-        
-        x=xTemp+xTwo
-        z=zTemp+zTwo
-    }
-    
-    x=(-z/x)
-    
-    print("x = \((round(x*1000))/1000)z")
-    
-    //Solve second equation
-    x = xOne*x
-    
-    zOne=zOne+x
-    
-    y=(-zOne/yOne)
-    
-    print("y = \((round(y*1000))/1000)z")
-}
-
-func assignment4 () {
+func assignment1 () {
     
     var n=0
     
@@ -250,7 +68,7 @@ func assignment4 () {
     }
 }
 
-func assignment5 () {
+func assignment2 () {
     
     var n=1
     
@@ -263,7 +81,7 @@ func assignment5 () {
     }
 }
 
-func assignment6 () {
+func assignment3 () {
     
     var n:Int = 1
     var answer: Int = 0
@@ -278,7 +96,7 @@ func assignment6 () {
     }
 }
 
-func assignment7 () {
+func assignment4 () {
     
     var n:Int = 1
     var answer: Int = 0
@@ -295,7 +113,8 @@ func assignment7 () {
     }
 }
 
-func assignment8 () {
+
+func assignment5 () {
     
     var n:Int = 1
     var answer: Int = 0
@@ -310,22 +129,7 @@ func assignment8 () {
     }
 }
 
-func assignment9 () {
-    
-    var n:Int = 1
-    var answer: Int = 0
-    
-    print ("Input a natural number: ",terminator:"" )
-    let number = Int(readLine() ?? "0") ?? 0
-
-    while (n <= number) {
-        answer=answer+n
-        n == (number-1) || n == number ? print("\(n) = \(answer)", terminator:"") : print("\(n) + ", terminator:"")
-        n+=2
-    }
-}
-
-func assignment10 () {
+func assignment6 () {
     
     var n:Int = 2
     var counter:Int = 0
@@ -346,7 +150,7 @@ func assignment10 () {
     }
 }
 
-func assignment11 () {
+func assignment7 () {
     
     var n:Int = 1
     
@@ -361,7 +165,7 @@ func assignment11 () {
     }
 }
 
-func assignment12 () {
+func assignment8 () {
     print ("Input a natural number: ",terminator:"" )
     let number = Int(readLine() ?? "0") ?? 0
     
@@ -375,7 +179,7 @@ func assignment12 () {
     print()
 }
 
-func assignment13 () {
+func assignment9 () {
     print ("Input a natural number: ",terminator:"" )
     let number = Int(readLine() ?? "0") ?? 0
     
@@ -393,7 +197,7 @@ func assignment13 () {
     }
 }
 
-func assignment14 () {
+func assignment10 () {
     print ("Input a natural number: ",terminator:"" )
     let number = Int(readLine() ?? "0") ?? 0
     
@@ -407,7 +211,7 @@ func assignment14 () {
     }
 }
 
-func assignment15 () {
+func assignment11 () {
     print ("Input number of rows: ",terminator:"" )
     let rows = Int(readLine() ?? "0") ?? 0
     
@@ -424,7 +228,7 @@ func assignment15 () {
     print()
 }
 
-func assignment16 () {
+func assignment12 () {
     print ("Input number of rows: ",terminator:"" )
     let rows = Int(readLine() ?? "0") ?? 0
     
@@ -445,7 +249,7 @@ func assignment16 () {
         }
 }
 
-func assignment17 () {
+func assignment13 () {
     print ("Input a natural number: ",terminator:"" )
     let number = Int(readLine() ?? "0") ?? 0
         
@@ -459,7 +263,7 @@ func assignment17 () {
         }
 }
 
-func assignment18 () {
+func assignment14 () {
     print ("Input a natural number: ",terminator:"" )
     let number = Int(readLine() ?? "0") ?? 0
     
@@ -478,7 +282,7 @@ func assignment18 () {
         }
 }
 
-func assignment19 () {
+func assignment15 () {
     print ("Input natural number: ",terminator:"" )
     let number = Int(readLine() ?? "0") ?? 0
     
@@ -517,7 +321,7 @@ func assignment19 () {
     }
 }
 
-func assignment20 () {
+func assignment16 () {
     print ("Input natural number: ",terminator:"" )
     let number = Int(readLine() ?? "0") ?? 0
     
@@ -585,3 +389,4 @@ func assignment20 () {
         print()
     }
 }
+
